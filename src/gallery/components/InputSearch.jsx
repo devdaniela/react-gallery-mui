aad2d2a2ef82333437712a81277fc3e557291f7c
Daniela Romero <clear>
2022-08-30 17:01:14 -0300

    aplicando un margin bottom al input search

--- src/gallery/components/InputSearch.jsx
+++ src/gallery/components/InputSearch.jsx
@@ -4,7 +4,7 @@ import { Box } from '@mui/system';
 
 export const InputSearch = () => {
   return (
-    <Grid container justifyContent="flex-end">
+    <Grid container justifyContent="flex-end" sx={{ mb: 2 }}>
         <Grid item xs={12} sx={{ mr: { xs: 0, md:15} }} textAlign="right">
             <InputLabel sx={{ display: {xs: 'none', sm:'inline-block'}, mt:1, mr:2 }}>Search by #</InputLabel>
             <TextField 
